532fc59f6e184f28e219bf3d2b381e7380336c78
cijang74 <82044367+cijang74@users.noreply.github.com>
2022-06-23 23:43:04 +0900

    6.23

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 이미지, 사운드 불러오기
 BackGround_image = pygame.image.load("Pictures/Back_Ground.png").convert() # 이미지
+playerTextureDefault=pygame.image.load('images/주인공_대기.png').convert() #오른방향 기본자세
+playerTextureDefault.set_colorkey(0,255,0)
+
+LplayerTextureDefault=pygame.image.load('images/주인공_대기_반전.png').convert() #왼방향 기본자세
+LplayerTextureDefault.set_colorkey(0,255,0)
+
+playerTexture=pygame.image.load('images/주인공_대기.png').convert()
+playerTexture.set_colorkey(0,255,0)
 #fishing_music = pygame.mixer.Sound('sounds/사운드 이름.wav') # 사운드
 
 # 클래스
@@ -27,8 +35,9 @@ class Character(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.x = 600
         self.y = 300
-        self.image = pygame.image.load("Pictures/Character.png").convert() # 이미지
-        self.image.set_colorkey((0, 181, 19))
+        self.image = pygame.image.load("Pictures/front_stand.png").convert() # 이미지
+        self.image = pygame.transform.scale(self.image, (256, 188))
+        # self.image.set_colorkey((0, 255, 0))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = (self.x,self.y)
         self.canMove_L = True #추가
@@ -38,21 +47,35 @@ class Character(pygame.sprite.Sprite):
 
         self.top = self.y
         self.left = self.x
-        self.bottom = self.y + 200
-        self.right = self.x + 125
+        self.bottom = self.y + 188
+        self.right = self.x + 256
+
+        self.lastInput = 0 #벽에 닿기 직전 사용자가 어떤 방향키를 누르고 벽에 닿았는지 좌우상하 순으로 1, 2, 3, 4로 저장
 
     def draw(self): # 플레이어 그리기
         screen.blit(self.image, (self.x, self.y))
 
     def move(self, rocks): # 플레이어의 이동을 담당하는 함수
-        self.character_rect_R = pygame.Rect(self.x+32, self.y+7, 5, 25) #추가
+        self.character_rect_R = pygame.Rect(self.x+32, self.y+7, 5, 25) #추가 left / top / 너비 / 높이
+
         self.character_rect_L = pygame.Rect(self.x+2, self.y+7, 5, 25) #추가
+
         self.character_rect_T = pygame.Rect(self.x+7, self.y+2, 25, 5) #추가
+
         self.character_rect_B = pygame.Rect(self.x+7, self.y+32, 25, 5) #추가
+        ################################################################################################ 40, 40 -> 256, 188
+        self.character_rect_R = pygame.Rect(self.x + 197, self.y + 25, 25, 113) #추가 left / top / 너비 / 높이
+
+        self.character_rect_L = pygame.Rect(self.x + 34, self.y + 25, 25, 113) #추가
+
+        self.character_rect_T = pygame.Rect(self.x+29, self.y, 113, 25) #추가
+        
+        self.character_rect_B = pygame.Rect(self.x+29, self.y+163, 113, 25) #추가
         ##이 부분부터 판정 알고리즘 수정
+
         wallCount = 0
         for wallCount in range(len(rocks)):
-            if self.character_rect_R.colliderect(rocks[wallCount].rect) == True:
+            if self.character_rect_R.colliderect(rocks[wallCount].wall_rect) == True:
                 self.canMove_R = False
                 break
             else:
@@ -60,7 +83,7 @@ class Character(pygame.sprite.Sprite):
 
         wallCount = 0
         for wallCount in range(len(rocks)):
-            if self.character_rect_L.colliderect(rocks[wallCount].rect) == True:
+            if self.character_rect_L.colliderect(rocks[wallCount].wall_rect) == True:
                 self.canMove_L = False
                 break
             else:
@@ -68,7 +91,7 @@ class Character(pygame.sprite.Sprite):
 
         wallCount = 0
         for wallCount in range(len(rocks)):
-            if self.character_rect_T.colliderect(rocks[wallCount].rect) == True:
+            if self.character_rect_T.colliderect(rocks[wallCount].wall_rect) == True:
                 self.canMove_U = False
                 break
             else:
@@ -76,7 +99,7 @@ class Character(pygame.sprite.Sprite):
 
         wallCount = 0
         for wallCount in range(len(rocks)):
-            if self.character_rect_B.colliderect(rocks[wallCount].rect) == True:
+            if self.character_rect_B.colliderect(rocks[wallCount].wall_rect) == True:
                 self.canMove_D = False
                 break
             else:
@@ -89,25 +112,24 @@ class Character(pygame.sprite.Sprite):
             self.canMove_D = True
         #여기까지 판단 알고리즘
 
-        if(self.canMove_L == True):
-            if pressed_keys[K_a] and self.x > 0: # 왼쪽
-                self.x -= 0.7 * dt
-                self.rect = pygame.Rect.move(self.rect,-(0.7 * dt), 0)
+        if pressed_keys[K_a] and self.x > 100 and self.canMove_L == True: # 왼쪽
+            self.x -= 0.7 * dt
+            self.rect = pygame.Rect.move(self.rect,-(0.7 * dt), 0)
+            self.lastInput = 1
 
-        if(self.canMove_R == True):
-            if pressed_keys[K_d] and self.x < 1920: # 오른쪽
-                self.x += 0.7 * dt
-                self.rect = pygame.Rect.move(self.rect,(0.7 * dt), 0)
+        elif pressed_keys[K_d] and self.x < 1564 and self.canMove_R == True: # 오른쪽
+            self.x += 0.7 * dt
+            self.rect = pygame.Rect.move(self.rect,(0.7 * dt), 0)
+            self.lastInput = 2 # 마지막으로 누른 키가 오른쪽이면 2번을 저장시킴
 
-        if(self.canMove_U == True):
-            if pressed_keys[K_w] and self.y > 0: # 위쪽
-                self.y -= 0.7 * dt
-                self.rect = pygame.Rect.move(self.rect,0, -(0.7 * dt))
+        elif pressed_keys[K_w] and self.y > 100 and self.canMove_U == True: # 위쪽
+            self.y -= 0.7 * dt
+            self.rect = pygame.Rect.move(self.rect,0, -(0.7 * dt))
+            self.lastInput = 3
 
-        if(self.canMove_D == True):
-            if pressed_keys[K_s] and self.y < 1080: # 아래
-                self.y += 0.7 * dt
-                self.rect = pygame.Rect.move(self.rect,0, (0.7 * dt))
+        elif pressed_keys[K_s] and self.y < 752 and self.canMove_D == True: # 아래
+            self.y += 0.7 * dt
+            self.rect = pygame.Rect.move(self.rect,0, (0.7 * dt))
 
 class Stage():
     def __init__(self):
@@ -123,16 +145,38 @@ class Rock(pygame.sprite.Sprite):
         self.y = y
         self.image = pygame.image.load("Pictures/Rock.png").convert() # 이미지
         self.image.set_colorkey((0, 255, 0))
+        self.image = pygame.transform.scale(self.image, (96, 96))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = (self.x,self.y)
+        self.rect.right, self.rect.bottom = (self.x + 64, self.y + 64)
         self.top = self.y
         self.left = self.x
         self.bottom = self.y + 80
         self.right = self.x + 80
+        self.wall_rect = pygame.Rect(self.x, self.y, 130, 130) #벽 범위
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
         
+class Enemy():
+        def __init__(self): # 플레이어 관련 데이터 초기화
+            pygame.sprite.Sprite.__init__(self)
+            self.x = 600
+            self.y = 300
+            self.image = pygame.image.load("Pictures/Character.png").convert() # 이미지
+            self.image.set_colorkey((0, 181, 19))
+            self.rect = self.image.get_rect()
+            self.rect.left, self.rect.top = (self.x,self.y)
+            self.canMove_L = True #추가
+            self.canMove_R = True #추가
+            self.canMove_U = True #추가
+            self.canMove_D = True #추가
+
+            self.top = self.y
+            self.left = self.x
+            self.bottom = self.y + 200
+            self.right = self.x + 125
+
 # 변수
 stage = Stage()
 character = Character()
